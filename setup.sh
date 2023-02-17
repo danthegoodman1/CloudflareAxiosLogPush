bab78cfa8e2ca33ae8d20519b58d0f717980d0b7
Dan Goodman <dan@danthegoodman.com>
2023-02-17 15:17:37 -0500

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
-curl -X POST 'https://api.cloudflare.com/client/v4/accounts/$ACCOUNTID/logpush/jobs' \
--H 'X-Auth-Key: $APIKEY' \
--H 'X-Auth-Email: $EMAIL' \
--H 'Content-Type: application/json' \
--d '{
-  "name": "workers-logpush",
-  "output_options": {
-    "field_names": ["ClientIP", "EdgeStartTimestamp", "RayID", "Event", "EventTimestampMs", "Outcome", "Exceptions", "Logs", "ScriptName"],
-    "sample_rate": 1.0,
-    "timestamp_format": "unixnano",
-    "output_type": "ndjson",
-    "batch_prefix": "[",
-    "batch_suffix": "]",
-    "record_prefix": "{",
-    "record_suffix": "}",
-    "record_delimiter": ","
+curl -X POST "https://api.cloudflare.com/client/v4/accounts/$ACCOUNTID/logpush/jobs" \
+-H "X-Auth-Key: $APIKEY" \
+-H "X-Auth-Email: $EMAIL" \
+-H "Content-Type: application/json" \
+-d "{
+  \"name\": \"workers-logpush\",
+  \"output_options\": {
+    \"field_names\": [\"ClientIP\", \"EdgeStartTimestamp\", \"RayID\", \"Event\", \"EventTimestampMs\", \"Outcome\", \"Exceptions\", \"Logs\", \"ScriptName\"],
+    \"sample_rate\": 1.0,
+    \"timestamp_format\": \"unixnano\",
+    \"output_type\": \"ndjson\",
+    \"batch_prefix\": \"[\",
+    \"batch_suffix\": \"]\",
+    \"record_prefix\": \"{\",
+    \"record_suffix\": \"}\",
+    \"record_delimiter\": \",\"
   },
-  "filter": "{\"key\":\"ScriptName\",\"operator\":\"!eq\",\"value\":\"cloudflareaxioslogpush\"}",
-  "destination_conf": "https://$SUBDOMAIN.workers.dev/log?header_Authorization=Bearer%20$TOKEN&header_content-type=application%2Fjson",,
-  "dataset": "workers_trace_events",
-  "enabled": true
-}'
+  \"filter\": \"{\\\"where\\\":{\\\"key\\\":\\\"ScriptName\\\",\\\"operator\\\":\\\"\!eq\\\",\\\"value\\\":\\\"cloudflareaxioslogpush\\\"}}\",
+  \"destination_conf\": \"https://$SUBDOMAIN.workers.dev/log?header_Authorization=Bearer%20$TOKEN&header_content-type=application%2Fjson\",
+  \"dataset\": \"workers_trace_events\",
+  \"enabled\": true
+}"
